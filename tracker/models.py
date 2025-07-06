d339e48a363acdb15bd1b809eba71cc5aa53e71c
okpranjalsingh <okpranjalsingh@gmail.com>
2025-07-06 20:18:03 +0530

    created model for Bugs

--- tracker/models.py
+++ tracker/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here
-
+from django.contrib.auth.models import User
 import uuid
 
 class Project(models.Model):
@@ -29,3 +29,22 @@ class Priority(models.Model):
     def __str__(self):
         return self.level
     
+
+
+class Bug(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    title = models.CharField(max_length=20)
+    description=models.TextField(max_length=200, null=True, blank=True)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE, related_name='bugs')
+    status = models.ForeignKey('Status', on_delete=models.CASCADE, related_name='bugs')
+    priority = models.ForeignKey('Priority', on_delete=models.CASCADE, related_name='bugs')
+
+    reported_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='reported_bugs')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.title
+
